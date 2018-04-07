b4a66ac137b1214750bea58052eb94fea7ed95d2
Graham Leva <g.ravel66@gmail.com>
2018-04-07 11:22:21 -0500

    Add notes to TODO and wave struct

---
+++ credo.md
@@ -0,0 +1,13 @@
+# Credo usage notes
+
+run credo:
+$ mix credo
+
+run credo and enforce style guides
+$ mix credo --strict
+
+Get a list of issues grouped by files instead of suggestions:
+$ mix credo list
+
+Get info on a specific file:
+$ mix credo <filename>
--- lib/wave.ex
+++ lib/wave.ex
@@ -2,6 +2,12 @@ defmodule Wave do
   @moduledoc """
   This module contains the Wave struct, which defines fields for the WAVE file 
   audio format.
+
+  Similar to hound's WavSpec, which defines:
+  * channels
+  * sample_ate
+  * bits_per_sample
+  * sample_format
   """
   defstruct [:chunk_id, :chunk_size, :format, :subchunk1_id, :subchunk1_size,
              :audio_format, :num_channels, :sample_rate, :byte_rate, :block_align,
