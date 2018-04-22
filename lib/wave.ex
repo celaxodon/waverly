742e28a44fa0cdd9d01e6b5f04819ad0deefc31f
Graham Leva <graham@contractsimply.com>
2018-04-22 13:39:58 -0500

    Move Wave struct functions into wave.ex
    * Update tests with the new Wave struct API

--- lib/wave.ex
+++ lib/wave.ex
@@ -33,7 +33,7 @@ defmodule Wave do
     bits_per_sample: nil,
     subchunk2_id: nil,
     subchunk2_size: nil,
-    audio_data: nil
+    audio_data: nil,
   ]
 
   def new(audio_data, format, sample_rate, channels) do
@@ -53,5 +53,105 @@ defmodule Wave do
       subchunk2_size: nil,
       audio_data: audio_data,
     }
+  end
+
+  @doc """
+  Reads a WAVE file and returns binary data with {:ok, data}, or {:error, reason}
+  `data` contains a struct with parsed informiation.
+  """
+  @spec read(String.t) :: {:ok, Wave.t} | {:error, String.t}
+  def read(path) do
+    with true <- String.ends_with?(path, ".wav"),
+         {:ok, data} <- File.read(path)
+    do
+      {:ok, data}
+    else
+      false -> {:error, "This library only accepts WAV files"}
+      {:error, :enoent} -> {:error, "#{path} does not exist"}
+      error -> error
+    end
+  end
+
+  @spec parse([char]) :: map
+  @doc """
+  `parse` returns a map of headers and data for bitstring file data
+  """
+  def parse(file_data) when is_bitstring(file_data) do
+    # TODO: Need to specify the type of the binary data, e.g., big-integer-size(32)?
+    # TODO: Handle RIFX (big-endian encoded) files. Not default, not sure how common this is...
+
+    ### RIFF (Resource Interchange File Format) chunk descriptor ###
+    # Should contain the letters "RIFF" in ASCII form (0x52494646)
+    # 4 bytes - (0-3) - big endian
+    <<chunk_id::32-big,
+      # 4 + (8 + subchunk1_size) + (8 + subchunk2_size) == 36 + subchunk2_size
+      # The size of the rest of the chunk following this number. The size of the
+      # whole file in bytes, minus 8 bytes for the two fields not included in the
+      # count: chunk_id and chunk_size
+      # 4 bytes - (4-7) - little endian
+      chunk_size::32-little,
+      # 4 bytes - (8-11) - big endian
+      # WAVE format contains two sub-chunks: "fmt" and "data"
+      # contains the letters "WAVE" (0x57415645 in big-endian)
+      format::32-big,
+      ### "fmt" subchunk  describing the format of the sound info in the data sub-chunk ###
+      # contains the letters "fmt" - (0x666d7420 big-endian form)
+      # 4 bytes (12-15) - big endian
+      subchunk1_id::32-big,
+      # 16 for PCM. The size of the rest of the subchunk that follows this number
+      # 4 bytes (16-19) - little endian
+      subchunk1_size::32-big,
+      # PCM = 1 (Linear quantization); values other than 1 indicate some form
+      # of compression
+      # 2 bytes (20-22) - little endian
+      audio_format::16-little,
+      # Mono = 1, Stereo = 2, etc.
+      # 2 bytes (22-23) - little endian
+      channels::16-little,
+      # E.g., 8000, 44_100, etc.
+      # 4 bytes (24-27) - little endian
+      sample_rate::32-little,
+      # equal to sample_rate * channels * bits_per_sample / 8
+      # 4 bytes - (28-31) - little endian
+      byte_rate::32-little,
+      # The number of bytes for one sample, including all channels.
+      # equal to channels * bits_per_sample / 8
+      # 2 bytes - (32-33) - little endian
+      block_align::16-little,
+      # 8 bits = 8, 16 bits = 16, etc.
+      # 2 bytes - (34-35) - little endian
+      bits_per_sample::16-little,
+      # TODO: Handle this possibility
+      # extra_param_size field - if PCM, this doesn't exist, otherwise 2 bytes
+      # extra_params - space for extra parameters
+      #
+      ### "data" subchunk - size of the sound info and contains raw sound data ###
+      #
+      # Contains the letters "data" (0x64617461 big endian form)
+      # 4 bytes - (36-39) - big endian
+      subchunk2_id::32-big,
+      # The number of bytes in the data. Can think of as the size of the read
+      # of the subchunk following this number
+      # equal to the num_samples * channels * bits_per_sample / 8
+      # 4 bytes (40-43) - little endian
+      subchunk2_size::32-little,
+      audio_data::little-binary>> = file_data # Actual sound data - little endian
+
+      %Wave{
+        chunk_id: chunk_id,
+        chunk_size: chunk_size,
+        format: format,
+        subchunk1_id: subchunk1_id,
+        subchunk1_size: subchunk1_size,
+        audio_format: audio_format,
+        channels: channels,
+        sample_rate: sample_rate,
+        byte_rate: byte_rate,
+        block_align: block_align,
+        bits_per_sample: bits_per_sample,
+        subchunk2_id: subchunk2_id,
+        subchunk2_size: subchunk2_size,
+        audio_data: audio_data
+      }
   end
 end
---
+++ test/wave_test.exs
@@ -0,0 +1,31 @@
+defmodule WaveTest do
+  use ExUnit.Case, async: true
+  doctest Wave
+
+  test "error messages on missing files or non WAV files" do
+    assert {:error, "test/fake_data.wav does not exist"} == Wave.read("test/fake_data.wav")
+    assert {:error, "This library only accepts WAV files"} == Wave.read("README.md")
+  end
+
+  test "we can read data from a WAV file" do
+    result = Wave.read("WAV_samples/PCM-16kHz/ex2-16kHz.wav")
+    assert elem(result, 0) == :ok
+    assert is_bitstring(elem(result, 1))
+  end
+
+  test "we can parse the header data from a sample 16-bit WAV file" do
+    {:ok, data} = Wave.read("WAV_samples/PCM-16kHz/ex2-16kHz.wav")
+    assert data != :nil
+    wave = Wave.parse(data)
+    # "RIFF" == 0x52, 0x49, 0x46, 0x46
+    assert Map.get(wave, :chunk_id) == 0x52494646
+    {riff_hex_val, _} = Integer.parse(Base.encode16("RIFF"), 16)
+    assert 0x52494646 == riff_hex_val
+    # "WAVE" == 0x57 0x41 0x56 0x45 == 57415645
+    assert Map.get(wave, :format) == 0x57415645
+    {wave_hex_val, _} = Integer.parse(Base.encode16("WAVE"))
+    assert 0x57415645 == wave_hex_val
+    # "DATA" == 0x44 0x41 0x54 0x41 == 44415441
+    assert Map.get(wave, :subchunk2_id) == 0x64617461
+  end
+end
